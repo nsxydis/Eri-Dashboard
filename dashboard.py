022ce246c8ccd7856d34e51ddd8089cdb030c998
Nick Xydis <131612274+nsxydis@users.noreply.github.com>
2024-01-26 10:57:32 -0600

    Created file

---
+++ dashboard.py
@@ -0,0 +1,16 @@
+'''
+Purpose: Main page for the dashboard.
+
+Author: Nick Xydis
+
+Version 1.0 - Programmer: Nick Xydis
+26Jan2024   - Created.
+'''
+
+import streamlit as st
+
+def main():
+    st.write("Hello World")
+
+if __name__ == "__main__":
+    main()
